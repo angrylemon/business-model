c859b08ce2c4016badab8e73ec5ead901563a466
angrylemon <veryangrylemon@gmail.com>
2014-07-13 01:57:20 +0300

    new goods added

--- src/company/Main.java
+++ src/company/Main.java
@@ -7,13 +7,14 @@ public class Main
 	{
 		Good lemon = new Good("Lemon");
 		Good apple = new Good("Apple");
-		
+		Good plate = new Good("Plate");
 		
  		Business bus1 = new Business(20000);
  		
  		
 		bus1.addGood(lemon, 30);
 		bus1.addGood(apple, 20);
+		bus1.addGood(plate, 12);
 		bus1.printGoods();
 		
 		
